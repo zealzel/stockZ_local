f549715980b04918e3c170331159f95d140c127f
Kitty <kitty@Kittyteki-MacBook-Air-2.local>
2014-01-21 15:07:12 +0800

    add folder traindata. mod core.py

--- core.py
+++ core.py
@@ -3,9 +3,12 @@ from datetime import date
 from dateutil import relativedelta as rdel
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
 # test samples
-tsAll=tsy,tsq,tsm,tsd=[pd.read_csv(data) for data in ['data_Y.csv','data_Q.csv','data_M.csv','data_D.csv']]
+files=['traindata/data_%s.csv' % str for str in ('Y','Q','M','D')]
+files[2]='traindata/data_M_initZero.csv'
+tsAll=tsy,tsq,tsm,tsd=[pd.read_csv(data) for data in files]
 valAll=valY,valQ,valM,valD=[t['value'].tolist() for t in tsAll]
 
 tsY,tsQ,tsM,tsD=[pd.DataFrame(v,index=pd.PeriodIndex(t['date'],freq=f),columns=['value']) \
@@ -13,8 +16,19 @@ tsY,tsQ,tsM,tsD=[pd.DataFrame(v,index=pd.PeriodIndex(t['date'],freq=f),columns=[
 
 class timeseries():
 # time series processor
+    
     def __init__(self,df):
         self.update(df)
+        self.period_years=3
+    
+    def setPeriodYears(self,pYears):
+        self.period_years=pYears
+    
+    def getPeriods(self,datetype):
+        pYears=self.period_years
+        periods=dict(zip(['Y','Q','M','D'], \
+                         [pYears,4*pYears,12*pYears,365*pYears]))
+        return periods[datetype]
     
     def update(self,df):
         self.df=df
@@ -28,12 +42,10 @@ class timeseries():
         return the DataFrame which is sorted and with periodindex filled in fixed range
         '''
         df_sorted=self.getSorted()
-        period_years=3
-        periods=dict(zip(['Y','Q','M','D'], \
-                         [period_years,4*period_years,12*period_years,365*period_years]))
+        self.getPeriods(datetype)
         
         last_period=df_sorted.index[-1]
-        first_period=last_period-periods[datetype]+1
+        first_period=last_period-self.getPeriods(datetype)+1
         
         prng=pd.period_range(first_period,last_period,freq=datetype)
         df_period=df_sorted.reindex(prng)
@@ -50,16 +62,40 @@ class timeseries():
         y=[y[i] for i in range(len(y)) if ~np.isnan(y[i])]
         return pd.DataFrame(y,index=x,columns=['value'])
     
-    def calRate(self,datetype):
+    def calRate(self,datetype,conservative=True):
         df_fit=self.getTimeSeriesForFit(datetype)
         x=df_fit.index.tolist()
         y=df_fit['value'].tolist()
-        A=np.matrix([[1]*len(x),np.array(x)-1]).T
-        b=np.matrix(np.log(y)).T
+        
+        # exclude the data point which value is zero 
+        xx=[x[i] for i in range(len(y)) if y[i]>0]
+        yy=[y[i] for i in range(len(y)) if y[i]>0]
+        
+        
+        A=np.matrix([[1]*len(xx),np.array(xx)-1]).T
+        b=np.matrix(np.log(yy)).T
         ans=(A.T*A).I*A.T*b
         a1=np.exp(np.array(ans)[0][0])
-        r=np.exp(np.array(ans)[0][0])-1
+        r=np.exp(np.array(ans)[1][0])-1
+        
+        # if the data is not complete, then multiply by a discount ratio to the growth rate
+        if conservative:
+            r*=len(xx)/float(self.getPeriods(datetype))
+        
         return [a1,r]
+    
+    def plotFit(self,datetype):
+        ans=self.calRate(datetype)
+        df_fit=self.getTimeSeriesForFit(datetype)
+        x=df_fit.index.tolist()
+        y=df_fit['value'].tolist()
+
+        f=lambda x : ans[0]*(1+ans[1])**(x-1)
+        x_fit=range(1,self.getPeriods(datetype)+1)
+        y_fit=f(np.array(x_fit))
+        
+        plt.scatter(x,y)
+        plt.plot(x_fit,y_fit)
     
     def getMean(self):
         return self.df.mean()['value']
